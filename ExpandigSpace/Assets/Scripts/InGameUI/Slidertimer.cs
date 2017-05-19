6845fbff07ff518522ae923551866602c2662af8
ColinvD <22950@ma-web.nl>
2017-05-19 12:01:21 +0200

    timer in scene colin added

---
+++ ExpandigSpace/Assets/Scripts/InGameUI/Slidertimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slidertimer : MonoBehaviour {
+    public Slider mainslider;
+    private float timer;
+    private float runningtime;
+	// Use this for initialization
+	void Start () {
+        mainslider.value = 240;
+        timer = 60;
+        runningtime = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(runningtime >= timer)
+        {
+            mainslider.value -= 1;
+            runningtime = 0;
+        } else
+        {
+            runningtime += Time.deltaTime*60;
+        }
+	}
+}
