0326516e9778b22a53ea91650aa0eb60b617230f
JeffrinDumas <jeffrin.e.d@live.nl>
2017-05-22 11:10:12 +0200

    rotation keys change

--- ExpandigSpace/Assets/Scripts/Scene_jeff/WalkingOnPlanet/PlayerController.cs
+++ ExpandigSpace/Assets/Scripts/Scene_jeff/WalkingOnPlanet/PlayerController.cs
@@ -23,12 +23,12 @@ public class PlayerController : MonoBehaviour {
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
       
        
-    if (Input.GetKey(KeyCode.Q))
+    if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, -Yspeed, 0));
         }
 
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(new Vector3(0, Yspeed, 0));
         }
