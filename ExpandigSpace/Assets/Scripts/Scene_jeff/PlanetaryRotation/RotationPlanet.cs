70d9cac649710294f752ece308815caa36cf63ef
JeffrinDumas <jeffrin.e.d@live.nl>
2017-05-22 10:54:04 +0200

    update

---
+++ ExpandigSpace/Assets/Scripts/Scene_jeff/PlanetaryRotation/RotationPlanet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationPlanet : MonoBehaviour
+{
+
+    [SerializeField]
+    private float Xspeed;
+    [SerializeField]
+    private float Zspeed;
+   
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(-Xspeed, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(Xspeed, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 0, Zspeed));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 0, -Zspeed));
+        }
+    }
+}
---
+++ ExpandigSpace/Assets/Scripts/Scene_jeff/WalkingOnPlanet/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float Yspeed;
+ 
+    float rotX;
+    float rotY;
+    
+
+    public float moveSpeed = 15;
+    private Vector3 moveDir;
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+      
+       
+    if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -Yspeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, Yspeed, 0));
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir * moveSpeed * Time.deltaTime));
+
+    }
+}
