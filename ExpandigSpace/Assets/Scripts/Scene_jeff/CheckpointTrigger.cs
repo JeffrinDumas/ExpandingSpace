ae98311cd1574be6880936159a18b57632f96094
JeffrinDumas <jeffrin.e.d@live.nl>
2017-06-28 12:07:22 +0200

    update not working Checkpoint

---
+++ ExpandigSpace/Assets/Scripts/Scene_jeff/CheckpointTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour 
+{
+	public List<Transform> checkpointPositions = new List<Transform>();
+	private int nextCheckpoint = 0;
+	public GameObject checkPrefab;
+	private Rigidbody rbody;
+
+	void Start () {
+      
+
+        rbody = GetComponent<Rigidbody>();
+        AddNextCheckpoint();
+        
+    }
+	
+	// Update is called once per frame
+
+	void OnTriggerEnter(Collider col)
+    {        
+        if (col.gameObject.tag != "Checkpoint")
+        {
+            return;
+        }
+        Debug.Log(col.gameObject);
+        Debug.Log(nextCheckpoint + ", " + checkpointPositions.Count);
+        GameObject.Destroy(col.gameObject);
+
+        if(nextCheckpoint < checkpointPositions.Count)
+        {
+            AddNextCheckpoint();
+        }
+            
+        
+
+    }
+	public void AddNextCheckpoint(){
+        
+        GameObject checkpoint = Instantiate(checkPrefab, checkpointPositions[nextCheckpoint].position, checkpointPositions[nextCheckpoint].rotation);
+        checkpoint.name = "checkpoint" + nextCheckpoint;
+        nextCheckpoint++;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up);
+        }
+    }
+}
+
+
